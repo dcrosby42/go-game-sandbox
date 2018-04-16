907a42d7269d043b53c1059025fc7333a794e90f
David Crosby <dcrosby@llnw.com>
2018-04-15 22:50:38 -0400

    conway tut complete

--- conway/main.go
+++ conway/main.go
@@ -5,8 +5,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/go-gl/gl/v3.3-core/gl"
 	_ "github.com/go-gl/gl/v4.1-core/gl"
@@ -14,10 +16,14 @@ import (
 )
 
 const (
-	width   = 500
-	height  = 500
-	rows    = 10
-	columns = 10
+	width  = 500
+	height = 500
+	fps    = 15
+
+	rows    = 100
+	columns = 100
+
+	threshold = 0.45
 )
 
 var (
@@ -44,7 +50,17 @@ func main() {
 	cells := makeCells()
 
 	for !window.ShouldClose() {
+		t := time.Now()
+
+		for r := range cells {
+			for _, c := range cells[r] {
+				c.checkState(cells)
+			}
+		}
+
 		draw(cells, window, program)
+
+		time.Sleep(time.Second/time.Duration(fps) - time.Since(t))
 	}
 }
 
@@ -165,14 +181,18 @@ func compileShader(source string, shaderType uint32) (uint32, error) {
 }
 
 type Cell struct {
-	Drawable uint32
-	NumPts   int32
-	X, Y     int
+	drawable         uint32
+	numPts           int32
+	x, y             int
+	alive, aliveNext bool
 }
 
 func (me Cell) Draw() {
-	gl.BindVertexArray(me.Drawable)
-	gl.DrawArrays(gl.TRIANGLES, 0, me.NumPts) //XXX int32(len(square)/3))
+	if !me.alive {
+		return
+	}
+	gl.BindVertexArray(me.drawable)
+	gl.DrawArrays(gl.TRIANGLES, 0, me.numPts) //XXX int32(len(square)/3))
 }
 
 func makeCells() [][]*Cell {
@@ -180,6 +200,8 @@ func makeCells() [][]*Cell {
 	for x := 0; x < rows; x++ {
 		for y := 0; y < columns; y++ {
 			c := newCell(x, y)
+			c.alive = rand.Float64() < threshold
+			c.aliveNext = c.alive
 			cells[x] = append(cells[x], c)
 		}
 	}
@@ -213,9 +235,71 @@ func newCell(x, y int) *Cell {
 	}
 
 	return &Cell{
-		Drawable: makeVao(points),
-		NumPts:   int32(len(square) / 3),
-		X:        x,
-		Y:        y,
+		drawable: makeVao(points),
+		numPts:   int32(len(square) / 3),
+		x:        x,
+		y:        y,
 	}
+}
+
+// checkState determines the state of the cell for the next tick of the game.
+func (c *Cell) checkState(cells [][]*Cell) {
+	c.alive = c.aliveNext
+	c.aliveNext = c.alive
+
+	liveCount := c.liveNeighbors(cells)
+	if c.alive {
+		// 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+		if liveCount < 2 {
+			c.aliveNext = false
+		}
+
+		// 2. Any live cell with two or three live neighbours lives on to the next generation.
+		if liveCount == 2 || liveCount == 3 {
+			c.aliveNext = true
+		}
+
+		// 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+		if liveCount > 3 {
+			c.aliveNext = false
+		}
+	} else {
+		// 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+		if liveCount == 3 {
+			c.aliveNext = true
+		}
+	}
+}
+
+// liveNeighbors returns the number of live neighbors for a cell.
+func (c *Cell) liveNeighbors(cells [][]*Cell) int {
+	var liveCount int
+	add := func(x, y int) {
+		// If we're at an edge, check the other side of the board.
+		if x == len(cells) {
+			x = 0
+		} else if x == -1 {
+			x = len(cells) - 1
+		}
+		if y == len(cells[x]) {
+			y = 0
+		} else if y == -1 {
+			y = len(cells[x]) - 1
+		}
+
+		if cells[x][y].alive {
+			liveCount++
+		}
+	}
+
+	add(c.x-1, c.y)   // To the left
+	add(c.x+1, c.y)   // To the right
+	add(c.x, c.y+1)   // up
+	add(c.x, c.y-1)   // down
+	add(c.x-1, c.y+1) // top-left
+	add(c.x+1, c.y+1) // top-right
+	add(c.x-1, c.y-1) // bottom-left
+	add(c.x+1, c.y-1) // bottom-right
+
+	return liveCount
 }
